c4259399e769779cf3a02a95d00fb5fc5ce8a4ef
thai le <thai.le@ist.psu.edu>
2020-10-20 17:10:32 -0400

    updates

--- sst/sst.py
+++ sst/sst.py
@@ -162,21 +162,21 @@ def main():
         averaged_grad = utils.get_average_grad(model, batch, trigger_token_ids)
 
         # pass the gradients to a particular attack to generate token candidates for each token.
-        cand_trigger_token_ids = attacks.hotflip_attack(averaged_grad,
-                                                        embedding_weight,
-                                                        trigger_token_ids,
-                                                        num_candidates=40,
-                                                        increase_loss=True)
+        # cand_trigger_token_ids = attacks.hotflip_attack(averaged_grad,
+        #                                                 embedding_weight,
+        #                                                 trigger_token_ids,
+        #                                                 num_candidates=40,
+        #                                                 increase_loss=True)
         # cand_trigger_token_ids = attacks.random_attack(embedding_weight,
         #                                                trigger_token_ids,
         #                                                num_candidates=40)
-        # cand_trigger_token_ids = attacks.nearest_neighbor_grad(averaged_grad,
-        #                                                        embedding_weight,
-        #                                                        trigger_token_ids,
-        #                                                        tree,
-        #                                                        100,
-        #                                                        num_candidates=40,
-        #                                                        increase_loss=True)
+        cand_trigger_token_ids = attacks.nearest_neighbor_grad(averaged_grad,
+                                                               embedding_weight,
+                                                               trigger_token_ids,
+                                                               tree,
+                                                               100,
+                                                               num_candidates=40,
+                                                               increase_loss=True)
 
         # Tries all of the candidates and returns the trigger sequence with highest loss.
         trigger_token_ids = utils.get_best_candidates(model,
